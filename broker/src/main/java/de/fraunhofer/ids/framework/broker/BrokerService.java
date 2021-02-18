29ce8e3422d4b410d65a3c832480bd96b91e8581
Tim Berthold <tim.berthold@isst.fraunhofer.de>
2021-02-18 09:15:09 +0100

    [BROKER] #REVIEW 'refactor:code-style' <issue-27> {Refactor code using Checkstyle-Plugin}

--- broker/src/main/java/de/fraunhofer/ids/framework/broker/BrokerService.java
+++ broker/src/main/java/de/fraunhofer/ids/framework/broker/BrokerService.java
@@ -5,14 +5,27 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
-import de.fraunhofer.iais.eis.*;
+import de.fraunhofer.iais.eis.DynamicAttributeToken;
+import de.fraunhofer.iais.eis.QueryLanguage;
+import de.fraunhofer.iais.eis.QueryScope;
+import de.fraunhofer.iais.eis.QueryTarget;
+import de.fraunhofer.iais.eis.Resource;
 import de.fraunhofer.iais.eis.ids.jsonld.Serializer;
 import de.fraunhofer.ids.framework.config.ClientProvider;
 import de.fraunhofer.ids.framework.config.ConfigContainer;
-import de.fraunhofer.ids.framework.daps.*;
+import de.fraunhofer.ids.framework.daps.ConnectorMissingCertExtensionException;
+import de.fraunhofer.ids.framework.daps.DapsConnectionException;
+import de.fraunhofer.ids.framework.daps.DapsEmptyResponseException;
+import de.fraunhofer.ids.framework.daps.DapsTokenManagerException;
+import de.fraunhofer.ids.framework.daps.DapsTokenProvider;
 import de.fraunhofer.ids.framework.messaging.util.IdsMessageUtils;
 import lombok.extern.slf4j.Slf4j;
-import okhttp3.*;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MultipartBody;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
 
@@ -37,7 +50,9 @@ public class BrokerService implements IDSBrokerService {
      * @param provider      providing underlying OkHttpClient
      * @param tokenProvider providing DAT Token for RequestMessage
      */
-    public BrokerService( ConfigContainer container, ClientProvider provider, DapsTokenProvider tokenProvider ) {
+    public BrokerService( final ConfigContainer container,
+                          final ClientProvider provider,
+                          final DapsTokenProvider tokenProvider ) {
         this.container = container;
         this.clientProvider = provider;
         this.tokenProvider = tokenProvider;
@@ -47,7 +62,7 @@ public class BrokerService implements IDSBrokerService {
      * {@inheritDoc}
      */
     @Override
-    public Response removeResourceFromBroker( String brokerURI, Resource resource )
+    public Response removeResourceFromBroker( final String brokerURI, final Resource resource )
             throws IOException, DapsTokenManagerException {
         logBuildingHeader();
 
@@ -68,7 +83,7 @@ public class BrokerService implements IDSBrokerService {
      * {@inheritDoc}
      */
     @Override
-    public Response updateResourceAtBroker( String brokerURI, Resource resource ) throws
+    public Response updateResourceAtBroker( final String brokerURI, final Resource resource ) throws
             IOException,
             DapsTokenManagerException {
         logBuildingHeader();
@@ -90,7 +105,7 @@ public class BrokerService implements IDSBrokerService {
      * {@inheritDoc}
      */
     @Override
-    public Response unregisterAtBroker( String brokerURI ) throws IOException, DapsTokenManagerException {
+    public Response unregisterAtBroker( final String brokerURI ) throws IOException, DapsTokenManagerException {
         logBuildingHeader();
 
         var securityToken = getDat();
@@ -109,7 +124,7 @@ public class BrokerService implements IDSBrokerService {
      * {@inheritDoc}
      */
     @Override
-    public Response updateSelfDescriptionAtBroker( String brokerURI ) throws
+    public Response updateSelfDescriptionAtBroker( final String brokerURI ) throws
             IOException, DapsTokenManagerException {
         logBuildingHeader();
 
@@ -129,7 +144,7 @@ public class BrokerService implements IDSBrokerService {
      * {@inheritDoc}
      */
     @Override
-    public List<Response> updateSelfDescriptionAtBrokers( List<String> brokerUris ) throws
+    public List<Response> updateSelfDescriptionAtBrokers( final List<String> brokerUris ) throws
             IOException, DapsTokenManagerException {
         var securityToken = getDat();
         var connectorID = getConnectorId();
@@ -167,8 +182,11 @@ public class BrokerService implements IDSBrokerService {
      * {@inheritDoc}
      */
     @Override
-    public Response queryBroker( String brokerURI, String query, QueryLanguage queryLanguage, QueryScope queryScope,
-                                 QueryTarget queryTarget ) throws IOException, DapsTokenManagerException {
+    public Response queryBroker( final String brokerURI,
+                                 final String query,
+                                 final QueryLanguage queryLanguage,
+                                 final QueryScope queryScope,
+                                 final QueryTarget queryTarget ) throws IOException, DapsTokenManagerException {
         logBuildingHeader();
 
         var securityToken = getDat();
@@ -194,7 +212,7 @@ public class BrokerService implements IDSBrokerService {
      *
      * @throws IOException if requestBody cannot be sent
      */
-    private Response sendBrokerMessage( String brokerURI, RequestBody requestBody ) throws IOException {
+    private Response sendBrokerMessage( final String brokerURI, final RequestBody requestBody ) throws IOException {
         var response = clientProvider.getClient().newCall(
                 new Request.Builder().url(brokerURI).post(requestBody).build()
         ).execute();
@@ -206,30 +224,69 @@ public class BrokerService implements IDSBrokerService {
         return response;
     }
 
+    /**
+     * Get the ID of the connector
+     *
+     * @return Connector-ID URI
+     */
     @NotNull
     private URI getConnectorId() {
         return container.getConnector().getId();
     }
 
+    /**
+     * Get a new DAT from the DAPS
+     *
+     * @return DAT, returned by the DAPS for the Connector
+     *
+     * @throws ConnectorMissingCertExtensionException Something went wrong with the Certificate of the Connector
+     * @throws DapsConnectionException                The DAPS is not reachable (wrong URL, network problems..)
+     * @throws DapsEmptyResponseException             The DAPS didn't return the expected response (maybe DAPS internal Problem?)
+     */
     private DynamicAttributeToken getDat()
             throws ConnectorMissingCertExtensionException, DapsConnectionException, DapsEmptyResponseException {
         return tokenProvider.getDAT();
     }
 
-    private MultipartBody getMessageBody( String header, String payload ) {
+    /**
+     * Build the MultiPart message with header and payload
+     *
+     * @param header  Header of the MultiPart message
+     * @param payload Payload of the MultiPart message
+     *
+     * @return Generated MultiPart Message
+     */
+    private MultipartBody getMessageBody( final String header, final String payload ) {
         return MessageBuilder.buildRequestBody(header, payload);
     }
 
+    /**
+     * Log info about starting to build the header
+     */
     private void logBuildingHeader() {
         log.debug("Building message header");
     }
 
-    private void logSendingMessage( String brokerURI ) {
+    /**
+     * Log info about starting to send the message to the Broker
+     *
+     * @param brokerURI URI of the Broker
+     */
+    private void logSendingMessage( final String brokerURI ) {
         log.debug(String.format("Sending message to %s", brokerURI));
     }
 
+    /**
+     * Serializes a given Resource
+     *
+     * @param resource The Resource to be serialized
+     *
+     * @return String, the serialized Resource
+     *
+     * @throws IOException Something went wrong reading the given Resource
+     */
     @NotNull
-    private String serializeResource( Resource resource ) throws IOException {
+    private String serializeResource( final Resource resource ) throws IOException {
         return SERIALIZER.serialize(resource);
     }
 }
--- broker/src/main/java/de/fraunhofer/ids/framework/broker/MessageBuilder.java
+++ broker/src/main/java/de/fraunhofer/ids/framework/broker/MessageBuilder.java
@@ -3,7 +3,21 @@ package de.fraunhofer.ids.framework.broker;
 import java.io.IOException;
 import java.net.URI;
 
-import de.fraunhofer.iais.eis.*;
+import de.fraunhofer.iais.eis.ConnectorUnavailableMessage;
+import de.fraunhofer.iais.eis.ConnectorUnavailableMessageBuilder;
+import de.fraunhofer.iais.eis.ConnectorUpdateMessage;
+import de.fraunhofer.iais.eis.ConnectorUpdateMessageBuilder;
+import de.fraunhofer.iais.eis.DynamicAttributeToken;
+import de.fraunhofer.iais.eis.QueryLanguage;
+import de.fraunhofer.iais.eis.QueryMessage;
+import de.fraunhofer.iais.eis.QueryMessageBuilder;
+import de.fraunhofer.iais.eis.QueryScope;
+import de.fraunhofer.iais.eis.QueryTarget;
+import de.fraunhofer.iais.eis.Resource;
+import de.fraunhofer.iais.eis.ResourceUnavailableMessage;
+import de.fraunhofer.iais.eis.ResourceUnavailableMessageBuilder;
+import de.fraunhofer.iais.eis.ResourceUpdateMessage;
+import de.fraunhofer.iais.eis.ResourceUpdateMessageBuilder;
 import de.fraunhofer.iais.eis.ids.jsonld.Serializer;
 import de.fraunhofer.ids.framework.messaging.util.IdsMessageUtils;
 import de.fraunhofer.ids.framework.util.MultipartDatapart;
@@ -16,20 +30,29 @@ public class MessageBuilder {
     private static final Serializer SERIALIZER = new Serializer();
 
     /**
+     * Utility classes (only static methods and fields) do not have a public constructor.
+     * Instantiating them does not make sense, prevent instantiating.
+     */
+    protected MessageBuilder() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
      * Create a ResourceUnavailableMessage used for unregistering the given resource at a broker
      *
-     * @param securityToken      the DAT Token used for this request
+     * @param securityToken    the DAT Token used for this request
      * @param infoModelVersion the Infomodel Version of the connector
-     * @param connectorID        the ID of the connector
-     * @param resource           the resource that is marked as unavailable at the broker
+     * @param connectorID      the ID of the connector
+     * @param resource         the resource that is marked as unavailable at the broker
      *
      * @return the {@link ResourceUnavailableMessage} as JSONLD
      *
      * @throws IOException when the message cannot be serialized properly
      */
-    public static String buildResourceUnavailableMessage( DynamicAttributeToken securityToken,
-                                                          String infoModelVersion, URI connectorID,
-                                                          Resource resource ) throws IOException {
+    public static String buildResourceUnavailableMessage( final DynamicAttributeToken securityToken,
+                                                          final String infoModelVersion,
+                                                          final URI connectorID,
+                                                          final Resource resource ) throws IOException {
         var msg = new ResourceUnavailableMessageBuilder()
                 ._affectedResource_(resource.getId())
                 ._securityToken_(securityToken)
@@ -44,17 +67,19 @@ public class MessageBuilder {
     /**
      * Create a ResourceUpdateMessage used for registering and updating the given resource at a broker
      *
-     * @param securityToken      the DAT Token used for this request
+     * @param securityToken    the DAT Token used for this request
      * @param infoModelVersion the Infomodel Version of the connector
-     * @param connectorID        the ID of the connector
-     * @param resource           the resource that is updated at the broker
+     * @param connectorID      the ID of the connector
+     * @param resource         the resource that is updated at the broker
      *
      * @return the {@link ResourceUpdateMessage} as JSONLD
      *
      * @throws IOException when the message cannot be serialized properly
      */
-    public static String buildResourceUpdateMessage( DynamicAttributeToken securityToken, String infoModelVersion,
-                                                     URI connectorID, Resource resource ) throws IOException {
+    public static String buildResourceUpdateMessage( final DynamicAttributeToken securityToken,
+                                                     final String infoModelVersion,
+                                                     final URI connectorID,
+                                                     final Resource resource ) throws IOException {
         var msg = new ResourceUpdateMessageBuilder()
                 ._affectedResource_(resource.getId())
                 ._securityToken_(securityToken)
@@ -69,16 +94,17 @@ public class MessageBuilder {
     /**
      * Create a ConnectorUnavailableMessage used for unregistering the connector at a broker
      *
-     * @param securityToken      the DAT Token used for this request
+     * @param securityToken    the DAT Token used for this request
      * @param infoModelVersion the Infomodel Version of the connector
-     * @param connectorID        the ID of the connector
+     * @param connectorID      the ID of the connector
      *
      * @return the {@link ConnectorUnavailableMessage} as JSONLD
      *
      * @throws IOException when the message cannot be serialized properly
      */
-    public static String buildUnavailableMessage( DynamicAttributeToken securityToken, String infoModelVersion,
-                                                  URI connectorID ) throws IOException {
+    public static String buildUnavailableMessage( final DynamicAttributeToken securityToken,
+                                                  final String infoModelVersion,
+                                                  final URI connectorID ) throws IOException {
         var msg = new ConnectorUnavailableMessageBuilder()
                 ._securityToken_(securityToken)
                 ._issued_(IdsMessageUtils.getGregorianNow())
@@ -93,16 +119,17 @@ public class MessageBuilder {
     /**
      * Create a ConnectorUpdateMessage used for registering the connector at a broker
      *
-     * @param securityToken      the DAT Token used for this request
+     * @param securityToken    the DAT Token used for this request
      * @param infoModelVersion the Infomodel Version of the connector
-     * @param connectorID        the ID of the connector
+     * @param connectorID      the ID of the connector
      *
      * @return the {@link ConnectorUpdateMessage} as JSONLD
      *
      * @throws IOException when the message cannot be serialized properly
      */
-    public static String buildUpdateMessage( DynamicAttributeToken securityToken, String infoModelVersion,
-                                             URI connectorID ) throws IOException {
+    public static String buildUpdateMessage( final DynamicAttributeToken securityToken,
+                                             final String infoModelVersion,
+                                             final URI connectorID ) throws IOException {
         var msg = new ConnectorUpdateMessageBuilder()
                 ._securityToken_(securityToken)
                 ._issued_(IdsMessageUtils.getGregorianNow())
@@ -117,20 +144,23 @@ public class MessageBuilder {
     /**
      * Create a QueryMessage used for querying the broker
      *
-     * @param securityToken      the DAT Token used for this request
+     * @param securityToken    the DAT Token used for this request
      * @param infoModelVersion the Infomodel Version of the connector
-     * @param connectorID        the ID of the connector
-     * @param queryLanguage      the Language of the Query (e.g. SPARQL, SQL, XQUERY)
-     * @param queryScope         the Scope of the Query (ALL connectors, ACTIVE connectors, INACTIVE connectors) {@link QueryScope}
-     * @param queryTarget        the type of IDS Components that are queried {@link QueryTarget}
+     * @param connectorID      the ID of the connector
+     * @param queryLanguage    the Language of the Query (e.g. SPARQL, SQL, XQUERY)
+     * @param queryScope       the Scope of the Query (ALL connectors, ACTIVE connectors, INACTIVE connectors) {@link QueryScope}
+     * @param queryTarget      the type of IDS Components that are queried {@link QueryTarget}
      *
      * @return the {@link QueryMessage} as JSONLD
      *
      * @throws IOException when the message cannot be serialized properly
      */
-    public static String buildQueryMessage( DynamicAttributeToken securityToken, String infoModelVersion,
-                                            URI connectorID, QueryLanguage queryLanguage, QueryScope queryScope,
-                                            QueryTarget queryTarget ) throws IOException {
+    public static String buildQueryMessage( final DynamicAttributeToken securityToken,
+                                            final String infoModelVersion,
+                                            final URI connectorID,
+                                            final QueryLanguage queryLanguage,
+                                            final QueryScope queryScope,
+                                            final QueryTarget queryTarget ) throws IOException {
         var msg = new QueryMessageBuilder()
                 ._securityToken_(securityToken)
                 ._issued_(IdsMessageUtils.getGregorianNow())
@@ -153,7 +183,7 @@ public class MessageBuilder {
      *
      * @return Two part multipart message containing the message header and payload as body
      */
-    public static MultipartBody buildRequestBody( String header, String payload ) {
+    public static MultipartBody buildRequestBody( final String header, final String payload ) {
         var builder = new MultipartBody.Builder();
         builder.setType(MultipartBody.FORM);
         builder.addFormDataPart(MultipartDatapart.HEADER.name(), header);
