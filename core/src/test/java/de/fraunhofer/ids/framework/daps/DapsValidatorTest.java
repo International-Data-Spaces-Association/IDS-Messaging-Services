d2cd89f47403d1d6318d0c8932f1546efdb4d689
eakker <erik.van.den.akker@isst.fraunhofer.de>
2021-03-10 09:10:21 +0100

    [CORE] #TEST 'function:ConfigProducerTest' {add test for ConfigProducer, add Maven Surefire to core pom}

---
+++ core/src/test/java/de/fraunhofer/ids/framework/daps/DapsValidatorTest.java
@@ -0,0 +1,14 @@
+package de.fraunhofer.ids.framework.daps;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DapsValidatorTest {
+
+    @Test
+    public void testTokenValidation(){
+
+    }
+
+}
---
+++ core/src/test/java/de/fraunhofer/ids/framework/util/MultipartParserTest.java
@@ -0,0 +1,14 @@
+package de.fraunhofer.ids.framework.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MultipartParserTest {
+
+    @Test
+    public void testParseMultipart(){
+
+    }
+
+}
