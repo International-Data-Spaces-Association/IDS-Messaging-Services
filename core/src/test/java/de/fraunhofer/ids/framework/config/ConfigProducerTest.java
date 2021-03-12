9b8740cfbf1ffc75eada6c2612164acf3f805263
eakker <erik.van.den.akker@isst.fraunhofer.de>
2021-03-12 13:00:10 +0100

    [CORE] #TEST 'file:ConfigProducerTest' {check if KeyManager and ClientProvider are initialized in ConfigProducer}

--- core/src/test/java/de/fraunhofer/ids/framework/config/ConfigProducerTest.java
+++ core/src/test/java/de/fraunhofer/ids/framework/config/ConfigProducerTest.java
@@ -25,6 +25,9 @@ class ConfigProducerTest {
     @Autowired
     private ConfigContainer configContainer;
 
+    @Autowired
+    private ClientProvider clientProvider;
+
     /**
      * SpringBootTest will initialize all Components in Core using the test/resources/application.properties file
      * then the test will check some fields of the loaded config.json of the imported configurationModel
@@ -35,6 +38,9 @@ class ConfigProducerTest {
         assertEquals(configModel.getId(), URI.create("https://w3id.org/idsa/autogen/configurationModel/a0b8bcea-6ba0-4e26-ba80-44e43ee058ac"));
         assertEquals(configModel.getConnectorDeployMode(), ConnectorDeployMode.TEST_DEPLOYMENT);
         assertEquals(configModel.getConnectorDescription().getId(), URI.create("https://w3id.org/idsa/autogen/baseConnector/691b3a17-0e91-4a5a-9d9a-5627772222e9"));
+        assertNotNull(clientProvider.getClient());
+        assertNotNull(configContainer.getKeyStoreManager().getCert());
+        assertNotNull(configContainer.getKeyStoreManager().getTrustManager());
     }
 
 }
