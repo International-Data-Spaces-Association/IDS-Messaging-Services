b8923bfbc47c22c91816a3993b62d9d5e77ac5c6
eakker <erik.van.den.akker@isst.fraunhofer.de>
2021-03-12 12:47:09 +0100

    [CORE] #TEST 'file:MultipartParser' {add test for parsing MultipartBody}

--- core/src/test/java/de/fraunhofer/ids/framework/util/MultipartParserTest.java
+++ core/src/test/java/de/fraunhofer/ids/framework/util/MultipartParserTest.java
@@ -1,14 +1,25 @@
 package de.fraunhofer.ids.framework.util;
 
+import okhttp3.MultipartBody;
+import okio.Buffer;
+import org.apache.commons.fileupload.FileUploadException;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MultipartParserTest {
 
     @Test
-    public void testParseMultipart(){
-
+    public void testParseMultipart() throws Exception {
+        var multipart = new MultipartBody.Builder().addFormDataPart("header", "value1").addFormDataPart("payload", "value2").build();
+        final Buffer buffer = new Buffer();
+        multipart.writeTo(buffer);
+        var multipartString = buffer.readUtf8();
+        var map = MultipartParser.stringToMultipart(multipartString);
+        assertEquals(map.get("header"), "value1");
+        assertEquals(map.get("payload"), "value2");
     }
 
 }
