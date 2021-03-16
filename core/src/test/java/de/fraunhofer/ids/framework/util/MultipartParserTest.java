33bcf88cb576afeb2ca0629786a72bec95e89544
eakker <erik.van.den.akker@isst.fraunhofer.de>
2021-03-16 09:33:29 +0100

    [CORE] #TEST 'file:DapsValidatorTest' {add Test for DAT Validation}

--- core/src/test/java/de/fraunhofer/ids/framework/util/MultipartParserTest.java
+++ core/src/test/java/de/fraunhofer/ids/framework/util/MultipartParserTest.java
@@ -1,14 +1,18 @@
 package de.fraunhofer.ids.framework.util;
 
+import lombok.extern.slf4j.Slf4j;
 import okhttp3.MultipartBody;
 import okio.Buffer;
 import org.apache.commons.fileupload.FileUploadException;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
 
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Slf4j
 class MultipartParserTest {
 
     @Test
@@ -17,6 +21,7 @@ class MultipartParserTest {
         final Buffer buffer = new Buffer();
         multipart.writeTo(buffer);
         var multipartString = buffer.readUtf8();
+        log.info(multipartString);
         var map = MultipartParser.stringToMultipart(multipartString);
         assertEquals(map.get("header"), "value1");
         assertEquals(map.get("payload"), "value2");
