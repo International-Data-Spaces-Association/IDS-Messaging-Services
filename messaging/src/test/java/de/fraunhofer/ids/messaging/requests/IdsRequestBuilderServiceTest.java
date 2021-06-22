663d26a9e243796b2d4bbc43b93cda69ce9cd61f
eakker <erik.van.den.akker@isst.fraunhofer.de>
2021-06-22 11:55:47 +0200

    [MESSAGING] #FIX 'file:IdsRequestBuilderService' {move expected payload to different constructor}

--- messaging/src/test/java/de/fraunhofer/ids/messaging/requests/IdsRequestBuilderServiceTest.java
+++ messaging/src/test/java/de/fraunhofer/ids/messaging/requests/IdsRequestBuilderServiceTest.java
@@ -1,6 +1,7 @@
 package de.fraunhofer.ids.messaging.requests;
 
 import de.fraunhofer.iais.eis.Artifact;
+import de.fraunhofer.iais.eis.Message;
 import de.fraunhofer.ids.messaging.core.daps.ClaimsException;
 import de.fraunhofer.ids.messaging.core.daps.DapsTokenManagerException;
 import de.fraunhofer.ids.messaging.protocol.multipart.parser.MultipartParseException;
@@ -23,11 +24,16 @@ class IdsRequestBuilderServiceTest {
 
     @Test
     public void test() throws DapsTokenManagerException, IdsRequestException, ClaimsException, MultipartParseException, IOException {
-        MessageContainer<?> response = idsRequestBuilderService
+        MessageContainer<Object> response = idsRequestBuilderService
                                                     .newRequest() //create a new RequestBuilder instance
                                                     .useTemplate(requestTemplateProvider.artifactRequestMessageMessageTemplate(URI.create("http://artifact"))) //mandatory: get a template (or create own Template using Lambda: () -> RequestMessage). Calling execute without set template leads to NoTemplateProvidedException.
                                                     .throwOnRejection() //optional: RejectionMessages will not be put into a MessageContainer, instead an IdsRequestException containing the RejectionReason will be thrown.
-                                                    .expectPayload(Artifact.class) //optional: (when IDS-LDP is complete and Multipart is Removed): try to parse payload to expected payload, if not possible UnexpectedPayloadException will be thrown. If not provided just pass ResponseBody InputStream as Payload.
                                                     .execute(URI.create("http://target")); //send message to target uri and put response into MessageContainer
+
+        MessageContainer<Artifact> resp = idsRequestBuilderService.newRequestExpectingType(Artifact.class)
+                                                    .useTemplate(requestTemplateProvider.artifactRequestMessageMessageTemplate(URI.create("http://artifact"))) //mandatory: get a template (or create own Template using Lambda: () -> RequestMessage). Calling execute without set template leads to NoTemplateProvidedException.
+                                                    .throwOnRejection() //optional: RejectionMessages will not be put into a MessageContainer, instead an IdsRequestException containing the RejectionReason will be thrown.
+                                                    .execute(URI.create("http://target")); //send message to target uri and put response into MessageContainer
+
     }
 }
