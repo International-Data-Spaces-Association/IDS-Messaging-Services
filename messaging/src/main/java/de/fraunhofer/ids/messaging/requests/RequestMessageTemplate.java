2751e8166dc04ac19cfad75e8807da67768e09bb
eakker <erik.van.den.akker@isst.fraunhofer.de>
2021-06-25 12:00:25 +0200

    [MESSAGING] #DOCS 'file:RequestMessageTemplate' {refactor template interface for checkstyle}

--- messaging/src/main/java/de/fraunhofer/ids/messaging/requests/RequestMessageTemplate.java
+++ messaging/src/main/java/de/fraunhofer/ids/messaging/requests/RequestMessageTemplate.java
@@ -5,5 +5,12 @@ import de.fraunhofer.ids.messaging.core.daps.DapsTokenManagerException;
 
 @FunctionalInterface
 public interface RequestMessageTemplate<T extends RequestMessage> {
+
+    /**
+     * Build the message defined by this template.
+     *
+     * @return built message by template
+     * @throws DapsTokenManagerException when no DAT for Message can be received
+     */
     T buildMessage() throws DapsTokenManagerException;
 }
