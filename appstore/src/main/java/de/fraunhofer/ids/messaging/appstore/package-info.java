61e988b419b1842acbcc638cd376de589ac80b80
Tim Berthold <tim.berthold@isst.fraunhofer.de>
2021-07-30 12:33:22 +0200

    chore: rename appstore package

--- appstore/src/main/java/de/fraunhofer/ids/messaging/appstore/package-info.java
+++ appstore/src/main/java/de/fraunhofer/ids/messaging/appstore/package-info.java
@@ -11,4 +11,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package de.fraunhofer.ids.framework.appstore;
+package de.fraunhofer.ids.messaging.appstore;
