f081cb2c65f747ff9128b864c09abccbd226abdd
Tim Berthold <75306992+tmberthold@users.noreply.github.com>
2022-02-17 10:33:28 +0100

    feat: add ConnectorFingerprintProvider for ski/aki fingerprint (#431)
    * feat: add ConnectorFingerprintProvider for ski/aki fingerprint
    
    * chore: fix checkstyle
    
    * chore: fix checkstyle

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,7 +9,10 @@
 # Changelog
 All notable changes to this project will be documented in this file.
 
-## Version [6.0.2] UNRELEASED
+## Version [6.1.0] UNRELEASED
+
+### Minor Change: New ConnectorFingerprintProvider
+- The static call `ConnectorFingerprintProvider.fingerprint` can be used to retrieve the aki/ski connector fingerprint from now on. This fingerprint is determined at the start of the connector based on its certificate. With each reload of the keystoremanager and thus potential change of the connector certificate, the entry is regenerated. If no valid connector certificate with the required aki/ski information is available, the `Optional<String> fingerprint` will be empty.
 
 ### Patch Change: Dependency Maintenance
 - Upgrade: org.apache.jena:jena-core 4.3.2 -> 4.4.0 ([PR 426](https://github.com/International-Data-Spaces-Association/IDS-Messaging-Services/pull/426))
--- core/src/main/java/de/fraunhofer/ids/messaging/core/daps/aisec/AisecTokenManagerService.java
+++ core/src/main/java/de/fraunhofer/ids/messaging/core/daps/aisec/AisecTokenManagerService.java
@@ -15,16 +15,14 @@ package de.fraunhofer.ids.messaging.core.daps.aisec;
 
 import java.io.IOException;
 import java.security.PrivateKey;
-import java.security.cert.X509Certificate;
 import java.time.Instant;
-import java.util.Arrays;
 import java.util.Date;
-import java.util.stream.Collectors;
 
 import de.fraunhofer.iais.eis.ConnectorDeployMode;
 import de.fraunhofer.ids.messaging.core.config.ClientProvider;
 import de.fraunhofer.ids.messaging.core.config.ConfigContainer;
 import de.fraunhofer.ids.messaging.core.config.ssl.keystore.KeyStoreManager;
+import de.fraunhofer.ids.messaging.core.config.util.ConnectorFingerprintProvider;
 import de.fraunhofer.ids.messaging.core.daps.ConnectorMissingCertExtensionException;
 import de.fraunhofer.ids.messaging.core.daps.DapsConnectionException;
 import de.fraunhofer.ids.messaging.core.daps.DapsEmptyResponseException;
@@ -39,17 +37,11 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
-import org.bouncycastle.asn1.ASN1OctetString;
-import org.bouncycastle.asn1.x509.AuthorityKeyIdentifier;
-import org.bouncycastle.asn1.x509.Extension;
-import org.bouncycastle.asn1.x509.SubjectKeyIdentifier;
 import org.jetbrains.annotations.NotNull;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
-
-import static org.apache.commons.codec.binary.Hex.encodeHexString;
 
 /**
  * Manages DAT requests to the DAPS.
@@ -99,19 +91,6 @@ public class AisecTokenManagerService implements TokenManagerService {
     @Value("${daps.jwt.signature.algorithm:RSA256}")
     private String signatureAlgorithm;
 
-    /***
-     * Beautifies Hex strings and will generate a result later used to
-     * create the client id (XX:YY:ZZ).
-     *
-     * @param hexString HexString to be beautified
-     * @return beautifiedHex result
-     */
-    private static String beautifyHex(final String hexString) {
-        return Arrays.stream(hexString.split("(?<=\\G..)"))
-                     .map(s -> s + ":")
-                     .collect(Collectors.joining());
-    }
-
     /**
      * Get the DAT from the DAPS at dapsURL using the current configuration.
      *
@@ -134,9 +113,12 @@ public class AisecTokenManagerService implements TokenManagerService {
         try {
             final var privateKey = getPrivateKey(keyStoreManager);
 
-            //This is the fingerprint of the connector, not the UUID (can als be called UID).
-            //
-            final var connectorFingerprint = getConnectorFingerprint(keyStoreManager);
+            //aki/ski fingerprint of the connector
+            final var fingerprint = ConnectorFingerprintProvider.fingerprint;
+            if (fingerprint.isEmpty()) {
+                throw new ConnectorMissingCertExtensionException("No fingerprint available!");
+            }
+            final var connectorFingerprint = fingerprint.get();
 
             if (log.isDebugEnabled()) {
                 log.debug("Retrieving Dynamic Attribute Token from DAPS... [code=(IMSCOD0104)]");
@@ -397,119 +379,6 @@ public class AisecTokenManagerService implements TokenManagerService {
                    .setIssuedAt(timeWithOffset)
                    .setAudience(targetAudience)
                    .setNotBefore(timeWithOffset);
-    }
-
-    /**
-     * Generates the fingerprint of the Connector (UID) using the KeyStoreManager.
-     *
-     * @param keyStoreManager The KeyStoremanager used to access the AKI and SKI of the certificate.
-     * @return The generated connector fingerprint (UID).
-     * @throws ConnectorMissingCertExtensionException Thrown if either AKI
-     * or SKI are not valid within the connector certificate.
-     */
-    @NotNull
-    private String getConnectorFingerprint(final KeyStoreManager keyStoreManager)
-            throws ConnectorMissingCertExtensionException {
-        final var certificate = getCertificate(keyStoreManager);
-        final var authorityKeyIdentifier = getCertificateAKI(certificate);
-        final var subjectKeyIdentifier = getCertificateSKI(certificate);
-
-        return generateConnectorFingerprint(authorityKeyIdentifier, subjectKeyIdentifier);
-    }
-
-    /**
-     * Generates the fingerprint of the connector (UID).
-     *
-     * @param authorityKeyIdentifier The connector certificate AKI.
-     * @param subjectKeyIdentifier The connector certificate SKI.
-     * @return The generated fingerprint of the connector (UID).
-     */
-    @NotNull
-    private String generateConnectorFingerprint(final byte[] authorityKeyIdentifier,
-                                                final byte[] subjectKeyIdentifier) {
-        final var akiResult = beautifyHex(encodeHexString(authorityKeyIdentifier).toUpperCase());
-        final var skiResult = beautifyHex(encodeHexString(subjectKeyIdentifier).toUpperCase());
-
-        return skiResult + "keyid:" + akiResult.substring(0, akiResult.length() - 1);
-    }
-
-    /**
-     * Get the SKI of the certificate.
-     *
-     * @param cert The X509Certificate-Certificate
-     * @return The SKI-KeyIdentifier of the certificate
-     * @throws ConnectorMissingCertExtensionException thrown if SKI of certificate is empty
-     */
-    private byte[] getCertificateSKI(final X509Certificate cert)
-            throws ConnectorMissingCertExtensionException {
-        if (log.isDebugEnabled()) {
-            log.debug("Get SKI from certificate... [code=(IMSCOD0110)]");
-        }
-
-        final var skiOid = Extension.subjectKeyIdentifier.getId();
-        final var rawSubjectKeyIdentifier = cert.getExtensionValue(skiOid);
-
-        if (rawSubjectKeyIdentifier == null) {
-            throw new ConnectorMissingCertExtensionException(
-                    "SKI of the Connector Certificate is null!");
-        }
-
-        final var ski0c = ASN1OctetString.getInstance(rawSubjectKeyIdentifier);
-        final var ski = SubjectKeyIdentifier.getInstance(ski0c.getOctets());
-
-        return ski.getKeyIdentifier();
-    }
-
-    /**
-     * Get the AKI of the certificate.
-     *
-     * @param cert The X509Certificate-Certificate
-     * @return The AKI-KeyIdentifier of the Certificate
-     * @throws ConnectorMissingCertExtensionException thrown if AKI of certificate is empty
-     */
-    private byte[] getCertificateAKI(final X509Certificate cert)
-            throws ConnectorMissingCertExtensionException {
-        if (log.isDebugEnabled()) {
-            log.debug("Get AKI from certificate... [code=(IMSCOD0111)]");
-        }
-
-        final var akiOid = Extension.authorityKeyIdentifier.getId();
-        final var rawAuthorityKeyIdentifier = cert.getExtensionValue(akiOid);
-
-        checkEmptyRawAKI(rawAuthorityKeyIdentifier); //can throw exception
-
-        final var akiOc = ASN1OctetString.getInstance(rawAuthorityKeyIdentifier);
-        final var aki = AuthorityKeyIdentifier.getInstance(akiOc.getOctets());
-
-        return aki.getKeyIdentifier();
-    }
-
-    /**
-     * Checks if AKI is empty.
-     *
-     * @param rawAuthorityKeyIdentifier The AKI to check
-     * @throws ConnectorMissingCertExtensionException thrown if AKI of certificate is null
-     */
-    private void checkEmptyRawAKI(final byte[] rawAuthorityKeyIdentifier)
-            throws ConnectorMissingCertExtensionException {
-        if (rawAuthorityKeyIdentifier == null) {
-            throw new ConnectorMissingCertExtensionException(
-                    "AKI of the Connector Certificate is null!");
-        }
-    }
-
-    /**
-     * Getting Certificate from KeyStoreManager.
-     *
-     * @param keyStoreManager The KeyStoreManager holding the Certificate
-     * @return The Certificate of the KeyStoreManager
-     */
-    private X509Certificate getCertificate(final KeyStoreManager keyStoreManager) {
-        if (log.isDebugEnabled()) {
-            log.debug("Getting Certificate from KeyStoreManager... [code=(IMSCOD0112)]");
-        }
-
-        return (X509Certificate) keyStoreManager.getCert();
     }
 
     /**
