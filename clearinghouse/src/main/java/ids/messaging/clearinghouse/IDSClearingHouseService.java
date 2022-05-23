52513a1feb893335477454b94a2c0b46628b858a
Tim Berthold <tim.berthold@sovity.de>
2022-05-23 12:10:13 +0200

    chore: add contributors to license headers

--- clearinghouse/src/main/java/ids/messaging/clearinghouse/IDSClearingHouseService.java
+++ clearinghouse/src/main/java/ids/messaging/clearinghouse/IDSClearingHouseService.java
@@ -1,4 +1,6 @@
 /*
+ * Copyright Fraunhofer Institute for Software and Systems Engineering
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -10,6 +12,10 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ *  Contributors:
+ *       sovity GmbH
+ *
  */
 package ids.messaging.clearinghouse;
 
