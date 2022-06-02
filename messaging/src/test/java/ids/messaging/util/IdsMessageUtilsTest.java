3c80e629c3d967ed42f0cefd5b41c0bc3682bf20
Tim Berthold <tim.berthold@sovity.de>
2022-06-02 09:10:23 +0200

    fix: major changes

--- messaging/src/test/java/ids/messaging/util/IdsMessageUtilsTest.java
+++ messaging/src/test/java/ids/messaging/util/IdsMessageUtilsTest.java
@@ -41,10 +41,10 @@ class IdsMessageUtilsTest {
     @Test
     void testUtils(){
         final var connector = new BaseConnectorBuilder()
-                ._curator_(URI.create("http://curator"))
+                ._curatorAsUri_(URI.create("http://curator"))
                 ._hasDefaultEndpoint_(new ConnectorEndpointBuilder()._accessURL_(URI.create("http://localhost")).build())
                 ._inboundModelVersion_(List.of("1.0.0"))
-                ._maintainer_(URI.create("http://maintainer"))
+                ._maintainerAsUri_(URI.create("http://maintainer"))
                 ._outboundModelVersion_("1.0.0")
                 ._securityProfile_(SecurityProfile.BASE_SECURITY_PROFILE)
                 .build();
